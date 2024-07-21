cb282d9286e539a912d429d5ad02963c93754729
20230912hahaha <liu88777@126.com>
2024-07-21 15:01:39 +0800

    篡改

--- code.c
+++ code.c
@@ -3,20 +3,6 @@
 
 int main(void)
 {
-	printf("testing code1..\n");
-	printf("testing code2..\n");
-	printf("testing code3..\n");
-	
-
-
-
-	printf("testing code4..\n");
-	printf("testing code45..\n");
-	
-
-
-	printf("testing code.33.\n");
-	printf("testing code222..\n");
-	printf("testing code1111..\n");
+	printf("change\n");
 	return 0;
 }
